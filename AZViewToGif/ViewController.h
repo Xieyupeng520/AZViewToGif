964f3f17719fbc57e50076c56de304472cca93ea
AZZ <806622085@qq.com>
2017-10-26 11:13:37 +0800

    Initial Commit

---
+++ AZViewToGif/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AZViewToGif
+//
+//  Created by cocozzhang on 2017/10/26.
+//  Copyright © 2017年 cocozzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
